7d500757dd2094baf54e8147badb1b43a0aa84c7
GoRhanHee <gorhanhee01@gmail.com>
2026-02-26 00:09:16 +0900

    build: edit import clang-r349610 code

--- build.sh
+++ build.sh
@@ -8,8 +8,14 @@ git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_a
  toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
 
 # Import clang-r349610
-git clone -b android10-release --depth 1 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 \
- toolchain/clang/host/linux-x86/clang-r349610-jopp
+TOOLCHAIN_URL="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android10-release/clang-r349610.tar.gz"
+CLANG_PATH="toolchain/clang/host/linux-x86/clang-r349610-jopp"
+mkdir -p "$CLANG_PATH"
+TOOLCHAIN_FILE=$(basename "$TOOLCHAIN_URL")
+if [ ! -f "$TOOLCHAIN_FILE" ]; then
+    wget -q --show-progress -O "$TOOLCHAIN_FILE" "$TOOLCHAIN_URL"
+fi
+tar -xf "$TOOLCHAIN_FILE" -C "$CLANG_PATH" && rm "$TOOLCHAIN_FILE"
 
 # OEM Setting
 export PLATFORM_VERSION=11
